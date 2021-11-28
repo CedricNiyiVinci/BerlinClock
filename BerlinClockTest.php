790710e38d38704c27c323bf7cf9d045842e8173
Cédric Niyikiza <cedric.niyikiza@student.vinci.be>
2021-11-28 11:49:51 +0100

    add - testSingleMinutesRowGiven59MinutesReturnYYYY

--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -5,14 +5,23 @@ use PHPUnit\Framework\TestCase;
 require "BerlinClock.php";
 class BerlinClockTest extends TestCase
 {
+    // given "00:00:00"
     public function testSingleMinutesRowGiven0MinutesReturnOOOO(){
         //Arrange
         $berlinClock = new BerlinClock();
         //Act
-
-        $actual = $berlinClock->singleMinutesRow();
+        $actual = $berlinClock->singleMinutesRow("00:00:00");
         //Assert
-
         $this ->assertEquals("OOOO" ,$actual);
+    }
+
+    // given "23:59:59"
+    public function testSingleMinutesRowGiven59MinutesReturnYYYY(){
+        //Arrange
+        $berlinClock = new BerlinClock();
+        //Act
+        $actual = $berlinClock->singleMinutesRow("23:59:59");
+        //Assert
+        $this ->assertEquals("YYYY" ,$actual);
     }
 }
