d897da5ed49bbb508189010c4d3ceb7998b9281d
Cédric Niyikiza <cedric.niyikiza@student.vinci.be>
2021-11-28 11:50:43 +0100

    add - extractMinutesFromTimestamp - test reussi

--- BerlinClock.php
+++ BerlinClock.php
@@ -2,8 +2,12 @@
 
 Class BerlinClock {
 
-    public function singleMinutesRow():string{
-        return "OOOO";
+    public function singleMinutesRow($parameter):string{
+        if($this->extractMinutesFromTimestamp($parameter)==0) return "OOOO";
+        return "YYYY";
     }
-
+    // 23:59:59 -> 59
+    public function extractMinutesFromTimestamp($timestamp):int{
+        return (int) substr($timestamp,3,4);
+}
 }
