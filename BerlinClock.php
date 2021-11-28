1fb2882d17ca73bbb4c4f8c185440df80623649a
AlexandreSGarcia <92111078+AlexandreSGarcia@users.noreply.github.com>
2021-11-28 13:10:34 +0100

    refactor -change singleMinutesRow guard with minute modulo 5 equals 4 return YYYY

--- BerlinClock.php
+++ BerlinClock.php
@@ -4,7 +4,7 @@ Class BerlinClock {
 
     public function singleMinutesRow($parameter):string{
         if($this->extractMinutesFromTimestamp($parameter)%5==0) return "OOOO";
-        if($this->extractMinutesFromTimestamp($parameter)==59 || $this->extractMinutesFromTimestamp($parameter)==54) return "YYYY";
+        if($this->extractMinutesFromTimestamp($parameter)%5==4) return "YYYY";
         if($this->extractMinutesFromTimestamp($parameter)==42) return "YYOO";
         if($this->extractMinutesFromTimestamp($parameter)==51) return "YOOO";
         return "YYYO";
